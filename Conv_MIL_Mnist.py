fe2958e0311bc79bea0b7910ae6d8a3d58b6257f
Dan Salo <dcs41@duke.edu>
2017-06-09 17:33:39 -0400

    Added additional fc layer and updated README to reflect the changes.

--- Conv_MIL_Mnist.py
+++ Conv_MIL_Mnist.py
@@ -20,22 +20,10 @@ from tensorflow.examples.tutorials.mnist import input_data
 from tqdm import tqdm
 
 
-# Global Dictionary of Flags
-flags = {  # data_directory not needed
-    'save_directory': 'summaries/',
-    'model_directory': 'conv_mil/',
-    'num_classes': 10,
-    'image_dim': 28,
-    'batch_size': 128,
-    'display_step': 500,
-    'weight_decay': 1e-7,
-}
-
-
 class ConvMil(Model):
-    def __init__(self, flags_input, run_num):
+    def __init__(self, flags_input):
         """ Initialize from Model class in TensorBase """
-        super().__init__(flags_input, run_num)
+        super().__init__(flags_input)
         self.valid_results = list()
         self.test_results = list()
         self.checkpoint_rate = 5  # save after this many epochs
@@ -47,14 +35,15 @@ class ConvMil(Model):
         self.num_train_images = self.mnist.train.num_examples
         self.num_valid_images = self.mnist.validation.num_examples
         self.num_test_images = self.mnist.test.num_examples
-        self.x = tf.placeholder(tf.float32, [None, flags['image_dim'], flags['image_dim'], 1], name='x')
-        self.y = tf.placeholder(tf.int32, shape=[None, flags['num_classes']], name='y')
+        self.x = tf.placeholder(tf.float32, [None, 28, 28, 1], name='x')
+        self.y = tf.placeholder(tf.int32, shape=[None, self.flags['NUM_CLASSES']], name='y')
 
     def _summaries(self):
         """ Write summaries out to TensorBoard. Called by TensorBase. """
-        tf.summary.scalar("Total Loss", self.cost)
-        tf.summary.scalar("Cross Entropy Loss", self.xentropy)
-        tf.summary.scalar("Weight Decay Loss", self.weight)
+        tf.summary.scalar("Total_Loss", self.cost)
+        tf.summary.scalar("XEntropy_Loss_Pi", self.xentropy_p)
+        tf.summary.scalar("XEntropy Loss_yi", self.xentropy_y)
+        tf.summary.scalar("Weight_Decay_Loss", self.weight)
 
     def _network(self):
         """ Define neural network. Uses Layers class of TensorBase. Called by TensorBase. """
@@ -68,29 +57,33 @@ class ConvMil(Model):
             net.conv2d(3, 128)
             net.conv2d(3, 128)
             net.maxpool()
-            net.conv2d(1, self.flags['num_classes'], activation_fn=tf.nn.sigmoid)
-            net.noisy_and(self.flags['num_classes'])
+            net.conv2d(1, self.flags['NUM_CLASSES'], activation_fn=tf.nn.sigmoid)
+            net.noisy_and(self.flags['NUM_CLASSES'])
+            self.P_i = net.get_output()
+            net.fc(self.flags['NUM_CLASSES'])
             self.y_hat = net.get_output()
             self.logits = tf.nn.softmax(self.y_hat)
 
     def _optimizer(self):
         """ Set up loss functions and choose optimizer. Called by TensorBase. """
-        const = 1/self.flags['batch_size']
-        self.xentropy = const * tf.reduce_sum(
-            tf.nn.softmax_cross_entropy_with_logits(labels=self.y, logits=self.y_hat, name='xentropy'))
-        self.weight = self.flags['weight_decay'] * tf.add_n(tf.get_collection('weight_losses'))
-        self.cost = tf.reduce_sum(self.xentropy + self.weight)
-        self.optimizer = tf.train.AdamOptimizer(learning_rate=flags['learning_rate']).minimize(self.cost)
+        const = 1/self.flags['BATCH_SIZE']
+        self.xentropy_p = const * tf.reduce_sum(
+            tf.nn.softmax_cross_entropy_with_logits(labels=self.y, logits=self.P_i, name='xentropy_p'))
+        self.xentropy_y = const * tf.reduce_sum(
+            tf.nn.softmax_cross_entropy_with_logits(labels=self.y, logits=self.y_hat, name='xentropy_y'))
+        self.weight = self.flags['WEIGHT_DECAY'] * tf.add_n(tf.get_collection('weight_losses'))
+        self.cost = tf.reduce_sum(self.xentropy_p + self.xentropy_y + self.weight)
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=self.flags['LEARNING_RATE']).minimize(self.cost)
 
     def train(self):
         """ Run training function for num_epochs. Save model upon completion. """
-        self.print_log('Training for %d epochs' % self.flags['num_epochs'])
+        print('Training for %d epochs' % self.flags['NUM_EPOCHS'])
 
-        for self.epoch in range(1, self.flags['num_epochs'] + 1):
+        for self.epoch in range(1, self.flags['NUM_EPOCHS'] + 1):
             for _ in tqdm(range(self.num_train_images)):
 
                 # Get minibatches of data
-                batch_x, batch_y = self.mnist.train.next_batch(flags['batch_size'])
+                batch_x, batch_y = self.mnist.train.next_batch(self.flags['BATCH_SIZE'])
                 batch_x = self.reshape_batch(batch_x)
 
                 # Run a training iteration
@@ -116,12 +109,12 @@ class ConvMil(Model):
         # Initialize to correct dataset
         print('Evaluating images in %s set' % dataset)
         if dataset == 'valid':
-            batch_x, batch_y = self.mnist.validation.next_batch(flags['batch_size'])
+            batch_x, batch_y = self.mnist.validation.next_batch(self.flags['BATCH_SIZE'])
             batch_x = self.reshape_batch(batch_x)
             num_images = self.num_valid_images
             results = self.valid_results
         else:
-            batch_x, batch_y = self.mnist.test.next_batch(flags['batch_size'])
+            batch_x, batch_y = self.mnist.test.next_batch(self.flags['BATCH_SIZE'])
             batch_x = self.reshape_batch(batch_x)
             num_images = self.num_test_images
             results= self.test_results
@@ -129,7 +122,7 @@ class ConvMil(Model):
         # Loop through all images in eval dataset
         for _ in tqdm(range(num_images)):
             logits = self.sess.run([self.logits], feed_dict={self.x: batch_x})
-            predictions = np.reshape(logits, [-1, self.flags['num_classes']])
+            predictions = np.reshape(logits, [-1, self.flags['NUM_CLASSES']])
             correct_prediction = np.equal(np.argmax(self.valid_batch_y, 1), np.argmax(predictions, 1))
             results = np.concatenate((results, correct_prediction))
 
@@ -142,15 +135,11 @@ class ConvMil(Model):
 
     def reshape_batch(self, batch):
         """ Reshape vector into image. Do not need if data that is loaded in is already in image-shape"""
-        return np.reshape(batch, [flags['batch_size'], self.flags['image_dim'], self.flags['image_dim'], 1])
-
-    def _print_metrics(self):
-        """ Helper function used by Model class in TensorBase """
-        self.print_log("Seed: %d" % flags['seed'])
+        return np.reshape(batch, [self.flags['BATCH_SIZE'], 28, 28, 1])
 
     def _record_train_metrics(self, loss):
         """ Records the metrics at every display_step iteration """
-        self.print_log("Batch Number: " + str(self.step) + ", Total Loss= " + "{:.6f}".format(loss))
+        print("Batch Number: " + str(self.step) + ", Total Loss= " + "{:.6f}".format(loss))
 
     def _record_eval_metrics(self, dataset):
         """ Record the accuracy on the eval dataset """
@@ -158,7 +147,7 @@ class ConvMil(Model):
             accuracy = np.mean(self.valid_results)
         else:
             accuracy = np.mean(self.test_results)
-        self.print_log("Accuracy on %s Set: %f" % (dataset, accuracy))
+        print("Accuracy on %s Set: %f" % (dataset, float(accuracy)))
         file = open(self.flags['restore_directory'] + dataset + 'Accuracy.txt', 'w')
         file.write('%s set accuracy:' % dataset)
         file.write(str(accuracy))
@@ -169,42 +158,30 @@ def main():
 
     # Parse Arguments
     parser = argparse.ArgumentParser(description='Faster R-CNN Networks Arguments')
-    parser.add_argument('-n', '--run_num', default=0)  # Saves all under /save_directory/model_directory/Model[n]
-    parser.add_argument('-e', '--epochs', default=1)  # Number of epochs for which to train the model
-    parser.add_argument('-r', '--restore', default=0)  # Binary to restore from a model. 0 = No restore.
-    parser.add_argument('-m', '--model_restore',default=1)  # Restores from /save_directory/model_directory/Model[n]
-    parser.add_argument('-f', '--file_epoch', default=1)  # Restore filename: 'part_[f].ckpt.meta'
-    parser.add_argument('-t', '--train', default=1)  # Binary to train model. 0 = No train.
-    parser.add_argument('-v', '--eval', default=1)  # Binary to evalulate model. 0 = No eval.
-    parser.add_argument('-l', '--learn_rate', default=1e-3)  # learning Rate
-    parser.add_argument('-i', '--vis', default=0)  # enable visualizations
-    parser.add_argument('-g', '--gpu', default=0)  # specifiy which GPU to use
-    args = vars(parser.parse_args())
-
-    # Set Arguments
-    flags['seed'] = 1234  # Random seed
-    flags['run_num'] = int(args['run_num'])
-    flags['num_epochs'] = int(args['epochs'])
-    flags['restore_num'] = int(args['model_restore'])
-    flags['file_epoch'] = int(args['file_epoch'])
-    flags['learning_rate'] = float(args['learn_rate'])
-    flags['vis'] = True if (int(args['vis']) == 1) else False
-    flags['gpu'] = int(args['gpu'])
-
-    # Choose to restore or not.
-    if args['restore'] == 0:
-        flags['restore'] = False
-    else:
-        flags['restore'] = True
-        flags['restore_file'] = 'part_' + str(args['file_epoch']) + '.ckpt.meta'
+    parser.add_argument('-n', '--RUN_NUM', default=0)  # Saves all under /save_directory/model_directory/Model[n]
+    parser.add_argument('-e', '--NUM_EPOCHS', default=1)  # Number of epochs for which to train the model
+    parser.add_argument('-r', '--RESTORE_META', default=0)  # Binary to restore from a model. 0 = No restore.
+    parser.add_argument('-m', '--MODEL_RESTORE', default=1)  # Restores from /save_directory/model_directory/Model[n]
+    parser.add_argument('-f', '--FILE_EPOCH', default=1)  # Restore filename: 'part_[f].ckpt.meta'
+    parser.add_argument('-t', '--TRAIN', default=1)  # Binary to train model. 0 = No train.
+    parser.add_argument('-v', '--EVAL', default=1)  # Binary to evaluate model. 0 = No eval.
+    parser.add_argument('-l', '--LEARNING_RATE', default=1e-3, type=float)  # learning Rate
+    parser.add_argument('-g', '--GPU', default=0)  # specify which GPU to use
+    parser.add_argument('-s', '--SEED', default=123)  # specify the seed
+    parser.add_argument('-d', '--MODEL_DIRECTORY', default='summaries/', type=str)  # To save all models
+    parser.add_argument('-a', '--SAVE_DIRECTORY', default='conv_mil/', type=str)  # To save individual run
+    parser.add_argument('-i', '--DISPLAY_STEP', default=500, type=int)  # how often to display metrics
+    parser.add_argument('-b', '--BATCH_SIZE', default=128, type=int)  # size of minibatch
+    parser.add_argument('-w', '--WEIGHT_DECAY', default=1e-7, type=float)  # decay on all Weight variables
+    parser.add_argument('-c', '--NUM_CLASSES', default=10, type=int)  # number of classes. proly hard code.
+    flags = vars(parser.parse_args())
 
     # Run model. Train and/or Eval.
-    model = ConvMil(flags, flags['run_num'])
-    if int(args['train']) == 1:
+    model = ConvMil(flags)
+    if int(flags['TRAIN']) == 1:
         model.train()
-    if int(args['eval']) == 1:
+    if int(flags['EVAL']) == 1:
         model.evaluate('test')
-
 
 if __name__ == "__main__":
     main()
--- README.md
+++ README.md
@@ -6,20 +6,35 @@ Deep Multiple Instance Learning](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC49
 The unique contribution of this paper is the noisyAND module.
 The implemented model uses the MNIST dataset for classification.
 
+### Paper Summary
+
+The architecture presented in the paper involves CNN, the noisyAND layer,
+followed by an additional fully connected layer with a joint cross entropy loss.
+
+![alt text][/png/arch.png]
+
+The joint cross entropy loss is summed over all classes and compares the
+output of the MIL layer, `P_i`, and the output of the additional fully connected layer, `y_i`,
+to the class layer for the entire image, `t_i`.
+
+![alt text][/png/loss.png]
+
 ### Dependencies
  * Python 3.5 or greater
  * Tensorflow 1.0 or greater
- * Current version of TensorBase (for Layers and Model class)
+ * Latest commit of TensorBase as of this writing: [d7be3da](https://github.com/dancsalo/TensorBase/commit/d7be3dafab9f88ee42b74eec4459c42fda6ba15c).
 
 ### Instructions
 To run the demo, clone the repo onto your local machine, navigate into the repository,
 using the command line/terminal, and type `python Conv_MIL_Mnist.py {argparser flags}`. 
  Replace `{argparser flags}` with the appropriate choice of options as described in
- the section below.
+ the section below. You will also have to clone TensorBase and place it in
+ the same folder as this repo.
 
 ### Argparser
 The following options are available for running the model:
-* `-n`, run_num, default = 0, saves all model files `under /save_directory/model_directory/Model[n]`
+
+* `-n`, run_num, default = 0, saves all model files under `/save_directory/model_directory/Model[n]`
 * `-e`, epochs, default = 1, number of epochs for which to train the model
 * `-r`, restore, default = 0, binary value indicating whether to restore from a model.
 * `-m`, model_restore, default = 1, restores from `/save_directory/model_directory/Model[n]`
@@ -27,5 +42,4 @@ The following options are available for running the model:
 * `-t`, train, default=1,  # Binary to train model. 0 = No train.
 * `-v`, eval, default=1)  # Binary to evalulate model. 0 = No eval.
 * `-l`, learn_rate, default=1e-3, learning rate
-* `-i`, vis, default = 0, binary value indicating whether to enable visualizations
-* `-g`, gpu, default = 0, options are [0, 1, 2]. `0` or `1` designates which GPU to use. `2` designates using all available GPUs.
+* `-g`, gpu, default = 0, accepts a single integer or a string "all" to use all available.
